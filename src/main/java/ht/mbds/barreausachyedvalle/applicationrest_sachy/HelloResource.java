69031447510b9e05fea70b72919a4be262ecba8d
Sebarreau <bsachyedvaelle@gmail.com>
2026-05-12 14:58:40 -0400

    Ajout endpoint avec parametre

--- src/main/java/ht/mbds/barreausachyedvalle/applicationrest_sachy/HelloResource.java
+++ src/main/java/ht/mbds/barreausachyedvalle/applicationrest_sachy/HelloResource.java
@@ -1,14 +1,23 @@
 package ht.mbds.barreausachyedvalle.applicationrest_sachy;
 
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 
+@RequestScoped
 @Path("/hello-world")
 public class HelloResource {
     @GET
     @Produces("text/plain")
     public String hello() {
         return "Hello, World!";
+    }
+    @GET
+    @Path("personnes/{nom}")
+    @Produces("text/plain")
+    public String helloPersonne(@PathParam("nom") String nom) {
+        return "Hello, " + nom;
     }
 }
